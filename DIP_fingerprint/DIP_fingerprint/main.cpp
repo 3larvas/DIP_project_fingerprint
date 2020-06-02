1d4d0789a98bd816ee706eea310befe8b81e01d2
3larvas <3larvas@gmail.com>
2020-06-03 01:21:45 +0900

    make main file

---
+++ DIP_fingerprint/DIP_fingerprint/main.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat image = imread("img/[1]_2019_8_4_L_I_1.bmp", 1);
+    imshow("original", image);
+    waitKey(0);
+    return 0;
+}
