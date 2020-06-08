90555114ad8400fb9e39c8aa2d65c1ee90d12de8
3larvas <3larvas@gmail.com>
2020-06-08 15:03:30 +0900

    edit parm name

--- DIP_fingerprint/DIP_fingerprint/main.cpp
+++ DIP_fingerprint/DIP_fingerprint/main.cpp
@@ -16,78 +16,62 @@ using namespace cv;
 
 int main() {
 	// ############### STEP 1. READ FILE         ###############
-	// ############### STEP 2. SEGMENTATION      ###############
-	// ############### STEP 3. NORMALIZATION     ###############
-	// ############### STEP 4. BLOCK ORIENTATION ###############
-	// ############### STEP 5. GABOR FILTER      ###############
-	// ############### STEP 6. BINARYIZATION     ###############
-	// ############### STEP 7. THINNING          ###############
-	// ############### STEP 7. DETECT MINUTIAE   ###############
-	// orientation block size
-	// rows, cols must be devided by block size
-	int block_size = 7;
-
 	Mat src = imread("img/[1]_2019_8_4_L_I_1.bmp");
 	Size size = { 152,200 };
+	int block_size = 7;
 	cvtColor(src, src, COLOR_RGB2GRAY);
-
-	// resize image
-	resize(src, src, size);
-
-	Mat segmented;
-	// segmantation image
-	Mat segmented2 = segmentation(src, segmented);
-	// normalize image
+	
+	// ############### STEP 2. NORMALIZATION     ###############
 	equalizeHist(src, src);
 
+	// ############### STEP 3. SEGMENTATION      ###############
+	Mat segmented;
+	Mat segmented2 = segmentation(src, segmented);	
 
-	// block oriented
+	// ############### STEP 4. BLOCK ORIENTATION ###############
 	pair<Mat, vector<pair<float, float>>> returned = orientation(src, block_size, false);
 	Mat show = returned.first;
 	vector<pair<float, float>> vec = returned.second;
-
 	pair<Mat, vector<pair<float, float>>> returned2 = orientation(src, 7, true);
 	Mat coredelta = returned2.first;
 
-	// gabor filter
+	// ############### STEP 5. GABOR FILTER      ###############
 	Mat gabored = gabor(src, vec, block_size) + segmented2;
 
+	// ############### STEP 6. BINARYIZATION     ###############
 	Mat gabored_end;
-	// binarization
 	threshold(gabored, gabored_end, 1, 255, THRESH_BINARY_INV);
 
-	// thinning
+	// ############### STEP 7. THINNING          ###############
 	Mat imgt = thinning(gabored_end);
 
-	// find minutiae and visual them
+	// ############### STEP 7. DETECT MINUTIAE   ###############
 	Mat result = printMinutiae(imgt, segmented2, vec, block_size, size, src);
 	// measure distance between ridge
 	//calculate(imgt, segmented2);
 
-	pyrUp(src, src);
-	imshow("src", src);
+	//pyrUp(src, src);
+	//imshow("src", src);
 
-	pyrUp(show, show);
-	imshow("show", show);
+	//pyrUp(show, show);
+	//imshow("show", show);
 
-	pyrUp(segmented2, segmented2);
-	imshow("segmented area", segmented2);
+	//pyrUp(segmented2, segmented2);
+	//imshow("segmented area", segmented2);
 
-	gabored.convertTo(gabored, CV_8U);
-	pyrUp(gabored, gabored);
-	imshow("gabored", gabored);
+	//gabored.convertTo(gabored, CV_8U);
+	//pyrUp(gabored, gabored);
+	//imshow("gabored", gabored);
 
-	imgt.convertTo(imgt, CV_8U);
-	pyrUp(imgt, imgt);
-	imshow("thinned", imgt);
+	//imgt.convertTo(imgt, CV_8U);
+	//pyrUp(imgt, imgt);
+	//imshow("thinned", imgt);
 
-	pyrUp(result, result);
-	imshow("check", result);
+	//pyrUp(result, result);
+	//imshow("check", result);
 
-	pyrUp(coredelta, coredelta);
-	imshow("coredelta", coredelta);
-
-	cout << "³¡!" << endl;
+	//pyrUp(coredelta, coredelta);
+	//imshow("coredelta", coredelta);
 
 	waitKey(0);
 	return 0;
