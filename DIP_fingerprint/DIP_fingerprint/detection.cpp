25494d3169a874c35e1ebc6776d1012a1315aa02
예환 <azss0470@gmail.com>
2020-06-11 17:13:51 +0900

    dection 수정

--- DIP_fingerprint/DIP_fingerprint/detection.cpp
+++ DIP_fingerprint/DIP_fingerprint/detection.cpp
@@ -1,7 +1,6 @@
 #include "detection.h"
-
 //variable for making bin file 
-int N_minutiae;   //manutiae Number
+int Num_minutiae;   //manutiae Number
 int I_width;   //image width
 int I_height;   //image height
 float angle_;
@@ -9,56 +8,52 @@ vector<unsigned char> m_angle;
 vector<unsigned char> m_type;
 vector<int> x_point;
 vector<int> y_point;
-
 /*==============================================================================================*
-function : detect
-desc     : 
+function : detect minutiae
+desc     : XOR 연산과 Minutiae의 특성을 이용하여 End Point와 Bifurcation Point를 구하고 이미지에 표시한다.
 		   
 parm
 	- minuiae	:  세선화 된(thinning) 입력영상
-	- mask		:  
-	- OrientMap	:  방향성을 갖춘 영상
+	- mask		:  세그멘테이션 필터
+	- OrientMap	:  Make bin file
 *==============================================================================================*/
-
 void detect(Mat& minutiae, Mat& mask, Mat& OrientMap)
 {
-	int end = 0, endcheck = 0;
-	int bif = 0, bifcheck = 0;
-	int found = 0;
-	Mat color2;
-	cvtColor(minutiae, color2, COLOR_GRAY2BGR);
+	int end = 0;
+	int endcheck = 0;
+	int bif = 0;
+	int bifcheck = 0;
+	int CN = 0;
+	Mat checked;
+	cvtColor(minutiae, checked, COLOR_GRAY2BGR);
 
-
-	int a, b, c, d, e;
+	// P9 P2 P3
+	// P8 P1 P4
+	// P7 P6 P5
+	int P1, P8, P6, P4, P2;
 	for (int x = 1; x < minutiae.rows - 1; x++) {
 		for (int y = 1; y < minutiae.cols - 1; y++) {
-			a = mask.at<uchar>(x, y);
-			b = mask.at<uchar>(x - 1, y);
-			c = mask.at<uchar>(x, y - 1);
-			d = mask.at<uchar>(x + 1, y);
-			e = mask.at<uchar>(x, y + 1);
+			P1 = mask.at<uchar>(x, y);
+			P8 = mask.at<uchar>(x - 1, y);
+			P6 = mask.at<uchar>(x, y - 1);
+			P4 = mask.at<uchar>(x + 1, y);
+			P2 = mask.at<uchar>(x, y + 1);
 
-			if (a != 0 && b != 0 && c != 0 && d != 0 && e != 0) {   //for not detecting point cut by mask because it is not ending point
-				found = 0;
-				if (minutiae.at<uchar>(x, y) == 255) {
-
-					if ((minutiae.at<uchar>(x - 1, y - 1)) != (minutiae.at<uchar>(x, y - 1))) found++;
-					if ((minutiae.at<uchar>(x, y - 1)) != (minutiae.at<uchar>(x + 1, y - 1))) found++;
-					if ((minutiae.at<uchar>(x + 1, y - 1)) != (minutiae.at<uchar>(x + 1, y))) found++;
-					if ((minutiae.at<uchar>(x + 1, y)) != (minutiae.at<uchar>(x + 1, y + 1))) found++;
-					if ((minutiae.at<uchar>(x + 1, y + 1)) != (minutiae.at<uchar>(x, y + 1))) found++;
-					if ((minutiae.at<uchar>(x, y + 1)) != (minutiae.at<uchar>(x - 1, y + 1))) found++;
-					if ((minutiae.at<uchar>(x - 1, y + 1)) != (minutiae.at<uchar>(x - 1, y))) found++;
-					if ((minutiae.at<uchar>(x - 1, y)) != (minutiae.at<uchar>(x - 1, y - 1))) found++;
-					/*found = minutiae.at<uchar>(x - 1, y + 1)/255 + minutiae.at<uchar>(x, y + 1)/255 + minutiae.at<uchar>(x + 1, y + 1)/255 +
-							minutiae.at<uchar>(x - 1, y)/255										+ minutiae.at<uchar>(x + 1, y)/255 +
-							minutiae.at<uchar>(x - 1, y - 1)/255 + minutiae.at<uchar>(x, y - 1)/255 + minutiae.at<uchar>(x + 1, y - 1)/255;*/
+			if (P1 != 0 && P8 != 0 && P6 != 0 && P4 != 0 && P2 != 0) {   //for not detecting point cut by mask because it is not ending point
+				CN = 0;
+				if (minutiae.at<uchar>(x, y) == 255) { // P1 == 1
+					if ((minutiae.at<uchar>(x, y + 1)) != (minutiae.at<uchar>(x - 1, y + 1))) CN++; // P2 - P9
+					if ((minutiae.at<uchar>(x - 1, y + 1)) != (minutiae.at<uchar>(x - 1, y))) CN++; // P9 - P8
+					if ((minutiae.at<uchar>(x - 1, y)) != (minutiae.at<uchar>(x - 1, y - 1))) CN++; // P8 - P7
+					if ((minutiae.at<uchar>(x - 1, y - 1)) != (minutiae.at<uchar>(x, y - 1))) CN++; // P7 - P6
+					if ((minutiae.at<uchar>(x, y - 1)) != (minutiae.at<uchar>(x + 1, y - 1))) CN++; // P6 - P5
+					if ((minutiae.at<uchar>(x + 1, y - 1)) != (minutiae.at<uchar>(x + 1, y))) CN++; // P5 - P4
+					if ((minutiae.at<uchar>(x + 1, y)) != (minutiae.at<uchar>(x + 1, y + 1))) CN++; // P4 - P3
+					if ((minutiae.at<uchar>(x + 1, y + 1)) != (minutiae.at<uchar>(x, y + 1))) CN++; // P3 - P2
 				}
 
-				if (found == 2) end = 1;
-				else if (found == 6) bif = 1;
-				//if (found == 1) end = 1;
-				//else if (found == 3) bif = 1;
+				if (CN == 2) end = 1;
+				else if (CN == 6) bif = 1;
 
 				//for make bin file
 				if (end || bif) {
@@ -69,39 +64,36 @@ void detect(Mat& minutiae, Mat& mask, Mat& OrientMap)
 					if (end) m_type.push_back(1);
 					else m_type.push_back(3);
 				}
-				//
 
 				if ((end)) {
 					Point pt1 = Point(y, x);
-					circle(color2, pt1, 3, Scalar(51, 255, 255), 1, 8, 0);   //yellow circle
+					circle(checked, pt1, 3, Scalar(51, 255, 255), 1, 8, 0);   //yellow circle
 					end = 0;
 					endcheck++;
 				}
 				if ((bif)) {
 					Point pt2 = Point(y, x);
-					circle(color2, pt2, 3, Scalar(0, 0, 255), 1, 8, 0);   //red circle
+					circle(checked, pt2, 3, Scalar(0, 0, 255), 1, 8, 0);   //red circle
 					bif = 0;
 					bifcheck++;
 				}
 			}
 		}
 	}
-	Mat show_color2 = color2;
-	resize(show_color2, show_color2, Size(300, 400));
-	imshow("Check_Minutiae", show_color2);
-	N_minutiae = endcheck + bifcheck;   //all minutiae number
-	cout << "end point Num : " << endcheck << endl;
-	cout << "bif point Num : " << bifcheck << endl;
-	cout << "minutiae of Number: " << N_minutiae << endl;
+	Mat show_checked = checked;
+	resize(show_checked, show_checked, Size(300, 400));
+	imshow("Check_Minutiae", show_checked);
+	Num_minutiae = endcheck + bifcheck;   //all minutiae number
+	cout << "End point Num : " << endcheck << endl;
+	cout << "Bifufcation point Num : " << bifcheck << endl;
+	cout << "minutiae of Number: " << Num_minutiae << endl;
 
-	int lim = 0;
+	int limit = 0;
 
-	for (int i = 0; i < N_minutiae; i++) {
-		if (i + lim >= 50) break;
+	for (int i = 0; i < Num_minutiae; i++) {
+		if (i + limit >= 50) break;
 		printf("X[%d]: %d Y[%d]: %d O[%d]: %d T[%d]: %d\n", i, x_point[i], i, y_point[i], i, m_angle[i], i, m_type[i]);
 	}
-
-
 
 }
 
